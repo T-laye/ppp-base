dd6f32201d532b240dd2599b3d5dacf0487f76a0
Laye <tlayefaah@gmail.com>
2024-02-15 10:02:06 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#3ED715",
+        // darkPrimary: "#191000",
+        // lighterGray: "#E9E9E9",
+        // lightGray: "#BFBFBF",
+        customGray: "#5C5F62",
+        error: "#EB5555",
       },
     },
   },
