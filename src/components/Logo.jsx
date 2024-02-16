141f4c26aa380680443ff399ba7b9c09e6613d27
Laye <tlayefaah@gmail.com>
2024-02-16 09:41:43 +0100

    admin sign in page

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div className="logo text-primary font-medium text-base">
+      <Link href="/">PPP-BASE</Link>
+    </div>
+  );
+}
--- src/components/SignHeader.jsx
+++ src/components/SignHeader.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Logo from "./Logo";
 
 export default function SignHeader({ signInPage }) {
   const router = useRouter();
@@ -12,9 +13,7 @@ export default function SignHeader({ signInPage }) {
 
   return (
     <header className="bg-white h-[7vh] shadow-sm px-4 flex justify-between items-center">
-      <div className="logo text-primary font-medium text-base">
-        <Link href="/">PPP-BASE</Link>
-      </div>
+     <Logo />
       <div>
         <button onClick={goToSignUp} className="btn bg-primary">
           {signInPage ? "Sign Up" : "Sign In"}
