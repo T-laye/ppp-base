a93bd6d07cdbe3e8d3303277768106d072fb632e
Laye <tlayefaah@gmail.com>
2024-02-28 14:12:17 +0100

    made customers list

--- src/app/admin/components/CustomerList.jsx
+++ src/app/admin/components/CustomerList.jsx
@@ -4,7 +4,11 @@ export default function CustomerList({ pending, name }) {
   return (
     <li className="flex mb-4 shadow bg-red-30 active:bg-primaryActive rounded-xl py-3 px-3 items-center justify-between">
       <div>{name}</div>
-      <button className={`btn ${pending ? "bg-primary" : "bg-customGray"} `}>
+      <button
+        className={`${
+          pending ? "btn bg-primary" : "btn-secondary bg-customGray"
+        } `}
+      >
         {pending ? "Approve" : "Enlist"}
       </button>
     </li>
