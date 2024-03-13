d5b4f7b7fc4d1d79ae212a272b080cb018c3e5e2
Laye <tlayefaah@gmail.com>
2024-03-13 12:15:55 +0100

    restructured management page

--- src/app/management/components/CustomerList.jsx
+++ src/app/management/components/CustomerList.jsx
@@ -6,7 +6,7 @@ export default function CustomerList({ pending, name, approved }) {
     <Link href="/management/customers/id">
       <div className="flex mb-4 border border-gray-200 bg-red-30 hover:bg-primaryActive active:border-primaryActive rounded-xl py-3 text-base px-3 items-center justify-between duration-200">
         <div>{name}</div>
-        {!approved && <button className="btn bg-primary">Create</button>}
+        {/* {!approved && <button className="btn bg-primary">Create</button>} */}
       </div>
     </Link>
   );
