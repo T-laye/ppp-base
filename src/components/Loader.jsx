0e8c3bf643942e697cf0f9960e4b0a07e81472d0
Laye <tlayefaah@gmail.com>
2024-03-13 17:51:57 +0100

    voucher verification page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import spinner from "/public/images/mini_spinner.png";
 import Image from "next/image";
+import { ImSpinner9 } from "react-icons/im";
 
 export default function Loader({ w = "6", h = "6" }) {
   return (
-    <div>
-      <Image
+    <div className="text-white animate-spin">
+      {/* <Image
         className={`animate-spin w-${w} h-${h}`}
         src={spinner}
         alt="Spinner"
@@ -14,7 +15,9 @@ export default function Loader({ w = "6", h = "6" }) {
         // aria-hidden="true"
         priority
         as="image"
-      />
+      /> */}
+
+      <ImSpinner9 size={28} />
     </div>
   );
 }
