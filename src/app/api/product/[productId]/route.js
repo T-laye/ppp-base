1fd0d196d84ba4268b8fa5f694e3dea9faa36549
Davidhero <owogogahhero@outlook.com>
2024-04-02 23:12:52 +0100

    update changes

---
+++ src/app/api/product/[productId]/route.js
@@ -0,0 +1,156 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../../config/prisma.connect";
+import { getAuthUser } from "../../../../../lib/get-auth-user";
+import ApiResponseDto from "../../../../../lib/apiResponseHelper";
+
+export async function GET(req, context) {
+  try {
+    const authResponse = getAuthUser(req, prisma, true);
+    if (authResponse.error) {
+      return NextResponse.json(
+        ApiResponseDto({
+          statusCode: authResponse.status,
+          message: authResponse.error.message,
+        }),
+        { status: authResponse.status }
+      );
+    }
+    const { params } = context;
+    const getProductId = params.productId;
+    const getProductById = await prisma.product.findUnique({
+      where: {
+        id: getProductId,
+      },
+      include: {
+        Voucher: true,
+        PointOfConsumption: true,
+        user: true,
+      },
+    });
+    if (!getProductById) {
+      return NextResponse.json(
+        ApiResponseDto({
+          statusCode: 404,
+          message: "product details not found",
+        }),
+        { status: 200 }
+      );
+    }
+    return NextResponse.json(
+      ApiResponseDto({
+        statusCode: 200,
+        data: getProductById,
+        message: "Successful",
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: err.message, error: err },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req, context) {
+  try {
+    const authResponse = getAuthUser(req, prisma, true);
+    if (authResponse.error) {
+      return NextResponse.json(
+        ApiResponseDto({
+          statusCode: authResponse.status,
+          message: authResponse.error.message,
+        }),
+        { status: authResponse.status }
+      );
+    }
+    const searchParams = req.nextUrl.searchParams;
+    const { params } = context;
+    const getProductId = params.productId;
+    const name = searchParams.get("productName");
+    const unit = searchParams.get("unit");
+    const voucherAllocation = searchParams.get("voucherAllocation");
+    const addJ = {
+      productName: name ? name : undefined,
+      unit: unit ? unit : undefined,
+      voucherAllocation: voucherAllocation ? voucherAllocation : undefined,
+    };
+
+    const updateProduct = await prisma.product.update({
+      where: {
+        id: getProductId,
+      },
+      data: addJ,
+    });
+    return NextResponse.json(
+      ApiResponseDto({
+        statusCode: 200,
+        data: updateProduct,
+        message: "successful",
+      }),
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { message: err.message, error: err },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req, context) {
+  try {
+    const authResponse = getAuthUser(req, prisma, true);
+    if (authResponse.error) {
+      return NextResponse.json(
+        ApiResponseDto({
+          statusCode: authResponse.status,
+          message: authResponse.error.message,
+        }),
+        { status: authResponse.status }
+      );
+    }
+    const { params } = context;
+    const getProductId = params.productId;
+    const getProductById = await prisma.product.findUnique({
+      where: {
+        id: getProductId,
+      },
+      include: {
+        Voucher: true,
+        PointOfConsumption: true,
+        user: true,
+      },
+    });
+    if (!getProductById) {
+      return NextResponse.json(
+        ApiResponseDto({
+          statusCode: 404,
+          message: "product details not found",
+        }),
+        { status: 200 }
+      );
+    }
+    await prisma.product.delete({
+      where: {
+        id: getProductId,
+      },
+      include: {
+        Voucher: true,
+        PointOfConsumption: true,
+      },
+    });
+    return NextResponse.json(
+      ApiResponseDto({
+        statusCode: 200,
+        message: "Successfully deleted product",
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: err.message, error: err },
+      { status: 500 }
+    );
+  }
+}
