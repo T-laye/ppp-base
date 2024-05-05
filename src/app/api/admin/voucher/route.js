845c65b638c3dbf279eb4898d583521769cde231
Davidhero <owogogahhero@outlook.com>
2024-05-05 22:21:13 +0100

    remove take

--- src/app/api/admin/voucher/route.js
+++ src/app/api/admin/voucher/route.js
@@ -292,9 +292,9 @@ export async function GET(req, res) {
     }
     const pageNumber = parseInt(searchParams.get("pageNumber"));
     const order = searchParams.get("order");
-    const take = searchParams.get("take")
-      ? parseInt(searchParams.get("take"))
-      : 10;
+    // const take = searchParams.get("take")
+    //   ? parseInt(searchParams.get("take"))
+    //   : 10;
     const verifiedBy = searchParams.get("verifiedBy");
     const product = searchParams.get("product_name");
     const collected = searchParams.get("collected");
@@ -361,7 +361,7 @@ export async function GET(req, res) {
           },
         },
       },
-      take: take,
+      // take: take,
       skip: offset,
       orderBy: {
         createdAt: "desc",
