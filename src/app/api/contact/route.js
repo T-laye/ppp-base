c90a38fecbe11b57979cc4e26cafc1958387d7a6
Davidhero <owogogahhero@outlook.com>
2024-05-14 17:58:44 +0100

    disabled

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
-import { sendEmailHelper } from "../../../../lib/email/email-transport";
-import WelcomeEmail from "../../../../lib/email/templates/welcome";
+// import { NextResponse } from "next/server";
+// import { sendEmailHelper } from "../../../../lib/email/email-transport";
+// import WelcomeEmail from "../../../../lib/email/templates/welcome";
 
-export async function GET(req, res) {
-  try {
-    const email = "davidhero125@gmail.com";
-    const emailRes = await sendEmailHelper({
-      email: email,
-      subject: "this is a test",
-      Body: WelcomeEmail,
-    });
+// export async function GET(req, res) {
+//   try {
+//     const email = "davidhero125@gmail.com";
+//     const emailRes = await sendEmailHelper({
+//       email: email,
+//       subject: "this is a test",
+//       Body: WelcomeEmail,
+//     });
 
-    if (emailRes.data) {
-      return NextResponse.json({ message: "successful" });
-    }
-    return NextResponse.json({ message: "ok" });
-  } catch (err) {
-    return NextResponse.json({ message: err });
-  }
-}
+//     if (emailRes.data) {
+//       return NextResponse.json({ message: "successful" });
+//     }
+//     return NextResponse.json({ message: "ok" });
+//   } catch (err) {
+//     return NextResponse.json({ message: err });
+//   }
+// }
