b29a88ea2102424ab9ce560cc6bb1e9ada94ff30
Tubolayefa <tlayefaah@gmail.com>
2024-06-04 22:04:50 +0100

    major changes

--- src/app/management/components/Header.jsx
+++ src/app/management/components/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Logo from "@/components/Logo";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsCardList, BsPersonFillGear } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
 import { BsPersonPlus } from "react-icons/bs";
@@ -13,25 +13,82 @@ import { MdVerifiedUser } from "react-icons/md";
 import { TbLogout2 } from "react-icons/tb";
 import { useLogoutMutation } from "@/redux/slices/usersApiSlice";
 import { logout } from "../../../redux/slices/authSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function Header() {
   const [openNav, setOpenNav] = useState(false);
-   const dispatch = useDispatch();
-   const router = useRouter();
-   const [logoutApiCall] = useLogoutMutation();
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [logoutApiCall] = useLogoutMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+  const [user, setUser] = useState({});
+  const [greeting, setGreeting] = useState("");
 
-   const logoutHandler = async () => {
-     try {
-       await logoutApiCall().unwrap();
-       dispatch(logout());
-       router.push("/");
+  // console.log(userInfo);
+
+  useEffect(() => {
+    const getWorkerDetails = async () => {
+      const res = await axios.get(`/api/admin/staff/${userInfo?.id}`);
+      // console.log(res?.data?.data);
+      setUser(res?.data?.data);
+    };
+
+    getWorkerDetails();
+  }, [userInfo?.id]);
+
+  useEffect(() => {
+    const getGreeting = () => {
+      const now = new Date();
+      const hours = now.getHours();
+
+      if (hours < 12) {
+        return "Good morning!";
+      } else if (hours < 18) {
+        return "Good afternoon!";
+      } else {
+        return "Good evening!";
+      }
+    };
+
+    setGreeting(getGreeting());
+  }, []);
+
+  function getLastWord(sentence) {
+    // if (typeof sentence !== "string") {
+    //   throw new Error("Input must be a string");
+    // }
+    if (user?.name) {
+      const words = sentence?.trim().split(/\s+/);
+      return words[words?.length - 1];
+    }
+  }
+
+  function capitalizeWords(sentence) {
+    // Split the sentence into an array of words
+    let words = sentence?.split(" ");
+
+    // Iterate over each word
+    for (let i = 0; i < words?.length; i++) {
+      // Capitalize the first letter of each word
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+
+    // Join the words back into a sentence
+    return words?.join(" ");
+  }
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      router.push("/");
       //  console.log("log out");
-     } catch (err) {
+    } catch (err) {
       //  console.log(err);
-     }
-   };
+    }
+  };
 
   const handleNav = () => {
     setOpenNav(!openNav);
@@ -40,10 +97,16 @@ export default function Header() {
     <header className=" lg:max-w-2xl mx-auto bg-white h-14 fixed top-0 left-0 right-0 shadow-sm    z-10">
       <div className="relative flex justify-between items-center px-4 py-1">
         <Logo />
-        <div className="cursor-pointer text-error" onClick={handleNav}>
-          {/* <IoIosMenu size={28} /> */}
-          <div>
-          <TbLogout2 size={28} onClick={logoutHandler} />
+        <div className="flex items-center gap-3">
+          {user?.gender && (
+            <p>
+              <span className="text-xl">👋</span> {greeting}{" "}
+              {user?.gender === "MALE" ? "Mr." : "Ma."}{" "}
+              {capitalizeWords(getLastWord(user?.name))}
+            </p>
+          )}
+          <div className="text-error cursor-pointer">
+            <TbLogout2 size={28} onClick={logoutHandler} />
           </div>
         </div>
       </div>
--- src/app/personnel/components/Header.jsx
+++ src/app/personnel/components/Header.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Logo from "@/components/Logo";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsCardList, BsPersonFillGear } from "react-icons/bs";
 import { FaListAlt } from "react-icons/fa";
 import { BsPersonPlus } from "react-icons/bs";
@@ -15,8 +15,9 @@ import { ImStatsBars } from "react-icons/im";
 import { RiAccountCircleFill } from "react-icons/ri";
 import { useLogoutMutation } from "@/redux/slices/usersApiSlice";
 import { logout } from "../../../redux/slices/authSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function Header() {
   const [openNav, setOpenNav] = useState(false);
@@ -26,6 +27,63 @@ export default function Header() {
   const dispatch = useDispatch();
   const router = useRouter();
   const [logoutApiCall] = useLogoutMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+  const [user, setUser] = useState({});
+  const [greeting, setGreeting] = useState("");
+
+  // console.log(userInfo);
+
+  useEffect(() => {
+    const getWorkerDetails = async () => {
+      const res = await axios.get(`/api/admin/staff/${userInfo?.id}`);
+      // console.log(res?.data?.data);
+      setUser(res?.data?.data);
+    };
+
+    getWorkerDetails();
+  }, [userInfo?.id]);
+
+  useEffect(() => {
+    const getGreeting = () => {
+      const now = new Date();
+      const hours = now.getHours();
+
+      if (hours < 12) {
+        return "Good morning!";
+      } else if (hours < 18) {
+        return "Good afternoon!";
+      } else {
+        return "Good evening!";
+      }
+    };
+
+    setGreeting(getGreeting());
+  }, []);
+
+  function getLastWord(sentence) {
+    // if (typeof sentence !== "string") {
+    //   throw new Error("Input must be a string");
+    // }
+    if (user?.name) {
+      const words = sentence?.trim().split(/\s+/);
+      return words[words?.length - 1];
+    }
+  }
+
+  function capitalizeWords(sentence) {
+    // Split the sentence into an array of words
+    let words = sentence?.split(" ");
+
+    // Iterate over each word
+    for (let i = 0; i < words?.length; i++) {
+      // Capitalize the first letter of each word
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+
+    // Join the words back into a sentence
+    return words?.join(" ");
+  }
 
   const logoutHandler = async () => {
     try {
@@ -42,71 +100,18 @@ export default function Header() {
     <header className=" lg:max-w-2xl mx-auto bg-white h-14 fixed top-0 left-0 right-0 shadow-sm    z-10">
       <div className="relative flex justify-between items-center px-4 py-1">
         <Logo />
-        <div className="cursor-pointer text-error">
-          {/* <IoIosMenu size={28} /> */}
-          <TbLogout2 size={28} onClick={logoutHandler} />
+        <div className="flex items-center gap-3">
+          {user?.gender && (
+            <p>
+              <span className="text-xl">👋</span> {greeting}{" "}
+              {user?.gender === "MALE" ? "Mr." : "Ma."}{" "}
+              {capitalizeWords(getLastWord(user?.name))}
+            </p>
+          )}
+          <div className="text-error cursor-pointer">
+            <TbLogout2 size={28} onClick={logoutHandler} />
+          </div>
         </div>
-        {/* <nav
-          onClick={handleNav}
-          className={` ${
-            openNav
-              ? "translate-x-0 duration-200 opacity-100"
-              : "duration-200 -translate-x-full opacity-0"
-          } absolute bg-[#b9b9b990] duration-200 backdrop-blur-sm h-screen top-0 left-0 right-0 bottom-0 z-[1000]`}
-        >
-          <ul className="text-xl font-normal h-full bg-white w-3/4 md:w-1/2 max-[300px]:w-11/12 rounded-tr-xl rounded-br-xl px-5 pt-3 ">
-            <div className="bg-gren-300 flex justify-start">
-              <Logo h="h-20" />
-            </div>
-            <div className="flex flex-col bg-ble-400 gap-5 mt-5 text-lg">
-              <Link legacyBehavior href="/personnel">
-                <li
-                  onClick={handleNav}
-                  className="flex items-center space-x-2 active:text-primary duration-200 hover:text-primary"
-                >
-                  <ImStatsBars size={20} />
-                  <a href="">Statistics</a>
-                </li>
-              </Link>
-              <Link legacyBehavior href="/verification">
-                <li
-                  onClick={handleNav}
-                  className="flex items-center space-x-2 active:text-primary duration-200 hover:text-primary"
-                >
-                  <MdVerifiedUser size={20} />
-                  <a href="">Verify Voucher</a>
-                </li>
-              </Link>
-              <Link legacyBehavior href="/usedVoucher">
-                <li
-                  onClick={handleNav}
-                  className="flex items-center space-x-2 active:text-primary duration-200 hover:text-primary"
-                >
-                  <FaHistory size={20} />
-                  <a href="">HIstory</a>
-                </li>
-              </Link>
-              <Link legacyBehavior href="/personnel/account">
-                <li
-                  onClick={handleNav}
-                  className="flex items-center space-x-2 active:text-primary duration-200 hover:text-primary"
-                >
-                  <RiAccountCircleFill size={20} />
-                  <a href="">Account</a>
-                </li>
-              </Link>
-              <Link legacyBehavior href="/">
-                <li
-                  onClick={handleNav}
-                  className="flex items-center mt-auto text-error space-x-2 active:text-primary duration-200 hover:text-primary"
-                >
-                  <TbLogout2 size={24} />
-                  <a href="">Sign Out</a>
-                </li>
-              </Link>
-            </div>
-          </ul>
-        </nav> */}
       </div>
     </header>
   );
